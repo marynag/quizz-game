a48f9a3bf369a3f9b7180aa0dbee89e02d69f8df
marynag <mgrabchak1109@gmail.com>
2025-05-28 23:35:34 +0300

    add question and answers

---
+++ src/components/QuestionCard/QuestionCard.tsx
@@ -0,0 +1,23 @@
+"use client";
+import React from "react";
+import { Question } from "@/types";
+
+import styles from "./QuestionCard.module.css";
+import { OptionTemplate } from "../common/OptionTemplate/OptionTemplate";
+
+type QuestionCardProps = {
+  question: Question;
+};
+
+export const QuestionCard = ({ question }: QuestionCardProps) => {
+  return (
+    <div className={styles.questionBg}>
+      <h2 className={styles.questionHeader}> {question.question}</h2>
+      <div className={styles.answersBox}>
+        {question.options.map((option, index) => (
+          <OptionTemplate key={index} text={option} number={index + 1} />
+        ))}
+      </div>
+    </div>
+  );
+};
